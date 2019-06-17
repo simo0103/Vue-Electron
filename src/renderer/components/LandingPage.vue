838925b8fdd6996c866f0a6deae443d6b20905dd
Simona Trotti <simona.trotti.external@ynap.com>
2019-06-17 17:14:58 +0200

    setup

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="wrapper">
+    
+    <main>
+     
+    </main>
+  </div>
+</template>
+
+<script>
+ // import SystemInformation from './LandingPage/SystemInformation'
+
+  // export default {
+  //   name: 'landing-page',
+  //   components: { SystemInformation },
+  //   methods: {
+  //     open (link) {
+  //       this.$electron.shell.openExternal(link)
+  //     }
+  //   }
+  // }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+</style>
