15c2d37ba577a7d14c5d62dbd436c30225c5eb8a
Simona Trotti <simona.trotti.external@ynap.com>
2019-08-14 17:40:30 +0200

    pulizia codice

--- src/renderer/components/Calendar/Calendar.vue
+++ src/renderer/components/Calendar/Calendar.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="calendar">
         <h1>CALENDAR</h1>
-        <!-- <h2>{{currentData}}</h2> -->
+        <h2>{{currentData}}</h2>
         <div class="nav-calendar">        
             <font-awesome-icon  class="prev" :icon="['fas', 'chevron-left']"></font-awesome-icon>
-            <p class= "month">{{getCurrentMonth}} {{getCurrentYear}}</p> 
+            <p class= "month">{{getCurrentMonth}} {{currentYear}}</p> 
             <font-awesome-icon  class="prev" :icon="['fas', 'chevron-right']"></font-awesome-icon> 
         </div>
         
         <ul>
           <li class="day"
                 v-bind:key="d.number"
-                :class= "d.number == getCurrentDay ? 'today' : ''"
+                :class= "d.number == currentDay ? 'today' : ''"
                 v-for="d in getActualMonthDays">
                 <span>{{ d.day }}</span> 
                 <span class="number"> {{ d.number }}</span>
@@ -26,52 +26,31 @@
 <script>
 export default {
   name: 'Calendar',
-   computed : {
-    getCurrentDay () {
-        const myDate = new Date(),
-              dayNumber = myDate.getDate();
-        return dayNumber;
+    data: function() {
+        return {
+            myDate : new Date(),
+            arrayOfDays : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'],
+            monthNames : ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"],
+            currentMonth : new Date().getMonth(),     
+            currentDay : new Date().getDate(),
+            currentYear : new Date().getFullYear(),
+        };
     },
+   computed : {
+    //nome mese
     getCurrentMonth () {
-        const myDate = new Date(),
-        currentMonth = myDate.getMonth(),
-        monthNames = ["January", "February", "March", "April", "May", "June",
-                          "July", "August", "September", "October", "November", "December"],
-        months = monthNames[myDate.getMonth()];
+        const months = this.monthNames[this.myDate.getMonth()];
         return months;
     },
-    getCurrentYear() {
-      const myDate = new Date(),  
-            currentYear = myDate.getFullYear();    
-        return currentYear;
-    },
-
-    next() {
-     const currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear,
-           currentMonth = (currentMonth + 1) % 12;
-           console.log(currentYear)
-    },
-    // years () {
-    //   const year = new Date().getFullYear()
-    //   return Array.from({length: year - 1900}, (value, index) => 1901 + index)
-    // }
-     allDays() { 
-      const arrayOfDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
-      const allDays = arrayOfDays;
-      return allDays;
-     },
     
     currentData() {
-      const myDate = new Date(),
-            dayNumber = myDate.getDate(),
-            dayOfTheWeek = myDate.getDay(),
-            currentMonth = myDate.getMonth(),
-            days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'],
-            monthNames = ["January", "February", "March", "April", "May", "June",
-                          "July", "August", "September", "October", "November", "December"],
-            months = monthNames[myDate.getMonth()],
-            currentYear = myDate.getFullYear();
-            let numberOfDays = new Date(currentYear, currentMonth + 1, 0).getDate(),
+      const dayNumber = this.currentDay,
+            dayOfTheWeek = this.myDate.getDay(),
+            
+            months = this.monthNames[this.myDate.getMonth()];
+           
+            let numberOfDays = new Date(this.currentYear, this.currentMonth + 1, 0).getDate(),
                 day = "";
             switch(dayOfTheWeek) {
                 case 0:
@@ -99,42 +78,39 @@ export default {
                     day = "";
             } 
       
-          const formattedDate = day + '/' + months + '/' + currentYear + '/' + numberOfDays;
+          const formattedDate = day + '/' + months + '/' + this.currentYear + '/' + numberOfDays;
       return formattedDate;
     },
+
     numberOFdaysInMonth() {
-      const myDate = new Date(),
-            currentMonth = myDate.getMonth(),
-            currentYear = myDate.getFullYear();
+      const currentMonth = this.myDate.getMonth(),
+            currentYear = this.myDate.getFullYear();
       let numberOfDays = new Date(currentYear, currentMonth + 1, 0).getDate(),
-          num = [],
-          days = [ 'Sunday', 'Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-          
+          num = [];          
           for(var i = 1; i <= numberOfDays ; i++) {
               console.log(i);             
               num.push(i);
               
           };
         
-      const numberOFdaysInMonth = num;
+       this.numberOFdaysInMonth = num;
       return numberOFdaysInMonth;
     },
 
     rightDay(today, day) {
-        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        var days = this.arrayOfDays,
             day = new Date(today.getFullYear(), today.getMonth(), day).getDay();
         return days[day % 7];
     },
 
     getActualMonthDays() {
-        var today = new Date(),
+        var today = this.myDate,
             numOfDays = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate(),
-            days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
             array = [];
         for(var i = 1; i <= numOfDays; i++) {
             var day = new Date(today.getFullYear(), today.getMonth(), i).getDay();
             array.push({
-                day: days[day % 7],
+                day: this.arrayOfDays[day % 7],
                 number: i
             });
         }
