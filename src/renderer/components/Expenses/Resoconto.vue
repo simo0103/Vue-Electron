798785ba7aa334587d1b3f373f4bc6ba32f97ad7
Simona Trotti <simona.trotti.external@ynap.com>
2019-08-28 17:48:00 +0200

    progress

--- src/renderer/components/Expenses/Resoconto.vue
+++ src/renderer/components/Expenses/Resoconto.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
         <h1>RESOCONTO</h1>
+        <div v-bind:key="post" v-for="post in posts">
+      <p>
+        <span><b>{{ post.title }}</b></span><br />
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
     </div>
 </template>
 <script>
+import PostsService from '../../services/PostsService'
+
 export default {
-  name: 'Resoconto' 
+  name: 'Resoconto',
+   data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
 }
 </script>
 <style>
